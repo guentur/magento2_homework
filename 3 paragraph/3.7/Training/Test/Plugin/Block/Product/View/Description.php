fbe0f23ffb73764460784cb48e3888312db5a18e
Kyrylo <batontram@gmail.com>
2021-03-10 00:54:09 +0300

    task 3.7 substitution tamplate file in custom plugin

---
+++ 3 paragraph/3.7/Training/Test/Plugin/Block/Product/View/Description.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Description
+ *
+ * @copyright Copyright © 2021 Peretiatko Kyrylo. All rights reserved.
+ * @author    batontramp@gmail.com
+ */
+
+namespace Training\Test\Plugin\Block\Product\View;
+
+
+class Description extends \Magento\Catalog\Block\Product\View\Description
+{
+    public function beforeToHtml(
+        \Magento\Catalog\Block\Product\View\Description $subject
+    ) {
+        $subject->setTemplate('Training_Test::description.phtml');
+    }
+}
+
