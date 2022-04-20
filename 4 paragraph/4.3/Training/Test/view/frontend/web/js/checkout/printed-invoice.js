9cfdf51bd8ba30c91b038f6fc81e2624bf8e49a0
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-04-20 12:55:47 +0300

    added task 4.3 solution

---
+++ 4 paragraph/4.3/Training/Test/view/frontend/web/js/checkout/printed-invoice.js
@@ -0,0 +1,11 @@
+define([
+    'jquery',
+    'uiComponent',
+    'ko'
+], function ($, Component, ko) {
+    'use strict';
+
+    return Component.extend({
+        // for possible additional functionality
+    });
+});
